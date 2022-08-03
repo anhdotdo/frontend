ba8032d4dc78ea47bccdb9e073fa6580a7207501
anhdotdo <dongocanh15798@gmail.com>
2022-08-03 16:03:15 +0700

    MODIFY App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>Hello, world</code> and save to reload.
+          Hello, world!
         </p>
         <a
           className="App-link"
