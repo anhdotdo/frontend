a541fdf8ff566b3afdfef5e6d69dd4c5e5026a2e
anhdotdo <dongocanh15798@gmail.com>
2022-08-03 19:05:15 +0700

    MODIFY App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello, world! I am Vietnamese 
+          Hello, world! I am VNese 
         </p>
         <a
           className="App-link"
