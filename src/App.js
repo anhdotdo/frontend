1ab8a5ed97c357304237b6651dda2856c60aa1d7
anhdotdo <dongocanh15798@gmail.com>
2022-08-03 16:07:49 +0700

    MODIFY App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello, world!
+          Hello, world! I am anh do, 
         </p>
         <a
           className="App-link"
